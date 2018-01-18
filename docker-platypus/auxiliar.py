22de7a2e91f245077a07473c8b9faa150a759d85
WendelHime <wendelhime@hotmail.com>
2018-01-18 17:06:16 -0200

    add port

--- docker-platypus/auxiliar.py
+++ docker-platypus/auxiliar.py
@@ -12,14 +12,17 @@ def replaceLine(filepath, regex, replace):
     fh.close()
     regex = re.compile(regex)
     content = regex.sub(replace, content)
+    print(replace)
     fh = open(filepath, "w")
     fh.writelines(content)
+    print(content)
     fh.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="This is a auxiliar script responsible to run result applications from report_html_db.pl in Docker.")
     parser.add_argument("organism", type=str, help="Organism first name")
     parser.add_argument("type", type=str, help="Application type to be runned", choices=['website', 'service'])
+    parser.add_argument("port", type=int, help="Port used to access application")
     parser.add_argument("--restendpoint", type=str, help="Services REST endpoint URL, if you're running website, this is required")
     parser.add_argument("--dbname", type=str, help="Database name")
     parser.add_argument("--dbhost", type=str, help="Database host URL")
@@ -34,7 +37,7 @@ if __name__ == "__main__":
         replaceLine("/" + args.organism + "-Website/lib/" + args.organism + "/Website/Model/Basic.pm", 
                 "dsn => 'dbi:SQLite:([/\w\-.]+)+'", 
                 "dsn => 'dbi:SQLite:/"+args.organism+"-Website/database.db'")
-        os.system("./"+args.organism + "-Website/script/" + args.organism.lower() + "_website_server.pl -p 80 -r")
+        os.system("./"+args.organism + "-Website/script/" + args.organism.lower() + "_website_server.pl -p " + str(args.port) + " -r")
     elif(args.type == "service"):    
         replaceLine("/" + args.organism + "-Services/lib/"+args.organism+"/Services/Model/SearchDatabaseRepository.pm", 
                 "dsn\s*=>\s*\"dbi:Pg:dbname=\w+;host=\w+\"", 
@@ -45,5 +48,5 @@ if __name__ == "__main__":
         replaceLine("/" + args.organism + "-Services/lib/"+args.organism+"/Services/Model/SearchDatabaseRepository.pm", 
                 "password\s*=>\s*\"\w+\"",
                 "password => \""+args.dbpassword+"\"")
-        os.system("./"+args.organism + "-Services/script/" + args.organism.lower() + "_services_server.pl -p 80 -r ")
+        os.system("./"+args.organism + "-Services/script/" + args.organism.lower() + "_services_server.pl -p " + str(args.port) + " -r ")
 
